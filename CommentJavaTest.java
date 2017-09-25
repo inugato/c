e732415303b5db869611aead078bde1c707b130f
inugato <longlamloi97@gmail.com>
2017-09-25 22:13:20 +0700

    updated

--- CommentJavaTest.java
+++
@@ -1,8 +0,0 @@
-class CommentJavaTest 
-{
-/*make a code line this is my second java program*/
-	public class void main (String []args)
-	{
-	     System.out.println("This is my second java program");
-	}
-}
